38a302060251bbe5de72490449539184e5b9d81e
Seeker90 <chris.carver90@icloud.com>
2025-10-29 01:50:56 +0000

    fixed contact list

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,18 +29,18 @@ export const Contact = () => {
           :
           store.contacts.map(contact => {
             return (
-              <div className="card d-inline-flex flex-row" key={contact.id}>
+              <div className="card d-inline-flex flex-row ms-3" key={contact.id}>
                 <ContactCard
                   name={contact.name}
                   address={contact.address}
                   phone={contact.phone}
                   email={contact.email}
                 />
-                <div className="ml-auto">
+                <div className="ms-auto mt-5 gap-3 me-5">
                   <Link to={`/update_contact/${contact.id}`}>
                     <button
                       type="button"
-                      className="btn btn-success flex items-center gap-2"
+                      className="btn btn-light  gap-4"
                       aria-label="Edit contact"
                     >
                       <svg
@@ -48,7 +48,7 @@ export const Contact = () => {
                         width="16"
                         height="16"
                         fill="currentColor"
-                        className="bi bi-pencil"
+                        className="bi bi-pencil ms-1"
                         viewBox="0 0 16 16"
                       >
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
@@ -57,7 +57,7 @@ export const Contact = () => {
                   </Link>
                   <button
                     type="button"
-                    className="btn btn-danger flex items-center gap-2"
+                    className="btn btn-light flex items-center gap-2"
                     onClick={() => handleDelete(contact.id)}
                     aria-label="Delete contact"
                   >
